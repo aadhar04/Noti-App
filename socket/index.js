365c3a2e883190e1156c7228efd2329b15a6677b
Aadhar Aggarwal <aadharoct@gmail.com>
2024-03-25 00:06:20 +0530

    first commit

---
+++ socket/index.js
@@ -0,0 +1,46 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let onlineUsers = [];
+
+const addNewUser = (username, socketId) => {
+  !onlineUsers.some((user) => user.username === username) &&
+    onlineUsers.push({ username, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (username) => {
+  return onlineUsers.find((user) => user.username === username);
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (username) => {
+    addNewUser(username, socket.id);
+  });
+
+  //when handleNotification() is called in card.js, sendNotification event is sent to server using socket.emit()
+  //then on server side server takes sendNotification event from client using socket.on.
+  //then send getNotification event is send to client using "io" .
+  //and then client recieve getNotification event from server using "socket.on" in "navbar.js"
+  socket.on("sendNotification", ({ senderName, receiverName, type }) => {
+    const receiver = getUser(receiverName);
+    io.to(receiver.socketId).emit("getNotification", {
+      senderName,
+      type,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
+
+io.listen(5000);
